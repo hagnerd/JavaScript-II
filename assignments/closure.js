e9f4643044c61e9d774c90b29c85c8e0cc3db879
Matt Hagner <web@matthagner.com>
2019-04-16 09:31:11 -0500

    Complete closure assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,28 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+// function add(a) {
+//  return function(b) {
+//    return a + b;
+//  }
+// }
+const add = a => b => a + b;
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 1;
+  return () => {
+    return count++;
+  };
 };
+
+let newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -18,4 +32,21 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+
+  return {
+    increment() {
+      count++;
+      return count;
+    },
+    decrement() {
+      count--;
+      return count;
+    }
+  };
 };
+
+const newNewCounter = counterFactory();
+console.log(newNewCounter.increment());
+console.log(newNewCounter.increment());
+console.log(newNewCounter.decrement());
