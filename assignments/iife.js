aa5904d5b9b713133da3ad2a690cc9b1c3bdd60e
Matt Hagner <web@matthagner.com>
2019-04-20 13:20:27 -0500

    Play with IIFE's more

--- assignments/iife.js
+++ assignments/iife.js
@@ -6,15 +6,14 @@
   console.log(a + b);
 })(10, 23);
 // even though vars are not block scoped, they are function scoped
-// IIFE's were an early way to have private date, and methods
+// IIFE's were an early way to have private data and methods
 // in JavaScript. internalVar can only be used within the IIFE above
 
 /*
- * Capture the return value from the iife
+ * You can capture the return value from the iife
  */
 let res = (function(a, b) {
   return a + b;
 })(83, 92);
 
 console.log(res);
-
